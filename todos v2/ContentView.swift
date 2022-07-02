d30cee9bd88a8547b48f57125ee2c78c1e288b26
Zhiyu <zhiyuma2006@gmail.com>
2022-07-02 09:11:21 +0800

    Initial Commit

---
+++ todos v2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  todos v2
+//
+//  Created by TinkerTanker on 2/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ todos v2/todos_v2App.swift
@@ -0,0 +1,17 @@
+//
+//  todos_v2App.swift
+//  todos v2
+//
+//  Created by TinkerTanker on 2/7/22.
+//
+
+import SwiftUI
+
+@main
+struct todos_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
